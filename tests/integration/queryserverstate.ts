a8abb47650761d229e3873d1d81f25ec24885802
Noah Jahn <najahn19@gmail.com>
2024-09-23 19:31:45 -0400

    Add tests for queryserverstate

--- tests/integration/queryserverstate.ts
+++ tests/integration/queryserverstate.ts
@@ -1,7 +1,33 @@
+import { expect } from 'chai';
+import { assertAndLog } from './index.js';
 import type { QueryServerStateResponseBody } from '../../src/functions/query-server-state/index.js';
-import SatisfactoryServer from '../../src/index.js';
+import SatisfactoryServer, { type ResponseBody } from '../../src/index.js';
 import logger from '../../src/logger/index.js';
 import loginAdministrator from './helpers/login-administrator.js';
+import assertBasicResponseStructure from './helpers/assert-basic-response-structure.js';
+
+function assertServerGameState({
+  queryServerState,
+  key,
+  type,
+}: {
+  queryServerState: ResponseBody<QueryServerStateResponseBody>;
+  key: string;
+  type: string;
+}) {
+  assertAndLog(
+    `The queryServerState's data object's serverGameState object has the ${key} property`,
+    () => {
+      expect(queryServerState.data.serverGameState).to.have.property(key);
+    },
+  );
+  assertAndLog(
+    `The queryServerState's data object's serverGameState object's ${key} property is a string`,
+    () => {
+      expect(queryServerState.data.serverGameState[key]).to.be.a(type);
+    },
+  );
+}
 
 async function test(satisfactoryServer: SatisfactoryServer) {
   await loginAdministrator(satisfactoryServer);
@@ -9,16 +35,85 @@ async function test(satisfactoryServer: SatisfactoryServer) {
     undefined,
     QueryServerStateResponseBody
   >('queryserverstate');
-  logger.debug(queryServerState.data.serverGameState.activeSessionName);
+  assertBasicResponseStructure(queryServerState);
+  assertAndLog(
+    "The queryServerState's data object has the serverGameState property",
+    () => {
+      expect(queryServerState.data).to.have.property('serverGameState');
+    },
+  );
+  assertAndLog(
+    "The queryServerState's data object's serverGameState property is an object",
+    () => {
+      expect(queryServerState.data.serverGameState.activeSessionName).to.be.an(
+        'object',
+      );
+    },
+  );
+  assertServerGameState({
+    queryServerState,
+    key: 'activeSessionName',
+    type: 'string',
+  });
+  assertServerGameState({
+    queryServerState,
+    key: 'numConnectedPlayers',
+    type: 'number',
+  });
+  assertServerGameState({
+    queryServerState,
+    key: 'playerLimit',
+    type: 'number',
+  });
+  assertServerGameState({
+    queryServerState,
+    key: 'techTier',
+    type: 'number',
+  });
+  assertServerGameState({
+    queryServerState,
+    key: 'activeSchematic',
+    type: 'string',
+  });
+  assertServerGameState({
+    queryServerState,
+    key: 'gamePhase',
+    type: 'string',
+  });
+  assertServerGameState({
+    queryServerState,
+    key: 'isGameRunning',
+    type: 'boolean',
+  });
+  assertServerGameState({
+    queryServerState,
+    key: 'totalGameDuration',
+    type: 'number',
+  });
+  assertServerGameState({
+    queryServerState,
+    key: 'isGameRunning',
+    type: 'boolean',
+  });
+  assertServerGameState({
+    queryServerState,
+    key: 'averageTickRate',
+    type: 'number',
+  });
+  assertServerGameState({
+    queryServerState,
+    key: 'autoLoadSessionName',
+    type: 'string',
+  });
 }
 
 async function execute() {
   logger.log('Testing Query server state...');
-  const satisfactorySecure = new SatisfactoryServer(
+  const satisfactoryServer = new SatisfactoryServer(
     `https://${process.env.SATISFACTORY_SERVER_BASE_URL}`,
   );
 
-  await test(satisfactorySecure);
+  await test(satisfactoryServer);
   logger.log('Query server state testing complete.');
 }
 
