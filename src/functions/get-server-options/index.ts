bd3ccf2c2b0dc91e9d9d30c398fe7e250f39d9a0
jam <jrlbidamin@gmail.com>
2024-09-27 12:51:33 -0400

    updated GetServerOptions types and removed tests that aren't guarenteed on other's servers

--- src/functions/get-server-options/index.ts
+++ src/functions/get-server-options/index.ts
@@ -1,14 +1,15 @@
 export type GetServerOptionsRequestData = undefined;
 
 export type GetServerOptionsResponseBody = {
-  serverOptions: Record<string, string> & {
-    'FG.DSAutoPause': 'True' | string;
-    'FG.DSAutoSaveOnDisconnect': 'True' | string;
-    'FG.AutosaveInterval': string;
-    'FG.ServerRestartTimeSlot': string;
-    'FG.SendGameplayData': 'True' | string;
-    'FG.NetworkQuality': string;
-  };
+  serverOptions: Record<string, string> &
+    Partial<{
+      'FG.DSAutoPause': 'False' | 'True' | string;
+      'FG.DSAutoSaveOnDisconnect': 'False' | 'True' | string;
+      'FG.AutosaveInterval': string;
+      'FG.ServerRestartTimeSlot': string;
+      'FG.SendGameplayData': 'False' | 'True' | string;
+      'FG.NetworkQuality': string;
+    }>;
   pendingServerOptions: Partial<GetServerOptionsResponseBody['serverOptions']>;
 };
 
--- tests/integration/getserveroptions.ts
+++ tests/integration/getserveroptions.ts
@@ -18,52 +18,6 @@ async function runTests(satisfactoryServer: SatisfactoryServer) {
   test("The getserveroptions's data object has the pendingServerOptions property", () => {
     expect(serveroptions.data).to.have.property('pendingServerOptions');
   });
-
-  test("The getserveroptions's data.serverOptions object's FG.DSAutoPause property is literal True, False or a string", () => {
-    expect(serveroptions.data.serverOptions['FG.DSAutoPause']).to.be.a(
-      'string',
-    );
-    expect(serveroptions.data.serverOptions['FG.DSAutoPause']).to.be.oneOf([
-      'True',
-      'False',
-    ]);
-  });
-
-  test("The getserveroptions's data.serverOptions object's FG.DSAutoSaveOnDisconnect property is literal True, False or a string", () => {
-    expect(
-      serveroptions.data.serverOptions['FG.DSAutoSaveOnDisconnect'],
-    ).to.be.a('string');
-    expect(
-      serveroptions.data.serverOptions['FG.DSAutoSaveOnDisconnect'],
-    ).to.be.oneOf(['True', 'False']);
-  });
-
-  test("The getserveroptions's data.serverOptions object's FG.AutosaveInterval property is a string", () => {
-    expect(serveroptions.data.serverOptions['FG.AutosaveInterval']).to.be.a(
-      'string',
-    );
-  });
-
-  test("The getserveroptions's data.serverOptions object's FG.ServerRestartTimeSlot property is a string", () => {
-    expect(
-      serveroptions.data.serverOptions['FG.ServerRestartTimeSlot'],
-    ).to.be.a('string');
-  });
-
-  test("The getserveroptions's data.serverOptions object's FG.SendGameplayData property is literal True, False or a string", () => {
-    expect(serveroptions.data.serverOptions['FG.SendGameplayData']).to.be.a(
-      'string',
-    );
-    expect(serveroptions.data.serverOptions['FG.SendGameplayData']).to.be.oneOf(
-      ['True', 'False'],
-    );
-  });
-
-  test("The getserveroptions's data.serverOptions object's FG.NetworkQuality property is a string", () => {
-    expect(serveroptions.data.serverOptions['FG.NetworkQuality']).to.be.a(
-      'string',
-    );
-  });
 }
 
 async function execute() {
